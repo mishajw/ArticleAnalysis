4967e318d6a9b97c574315cf1893d47e81bdde00
Misha Wagner <mishajw@gmail.com>
2016-04-01 18:49:20 +0100

    Documentation, and fixed averaging bug

--- WordCountClustering.hs
+++ WordCountClustering.hs
@@ -5,32 +5,39 @@ import WordCount (wordCount)
 
 import Data.List
 
+-- |Run the clustering on a list of text
 runOnText :: [String] -> [[[(String, Int)]]]
 runOnText ss =
   let wcs = map wordCount ss in
-  Clustering.kmeans cmpWordCount avgWordCount 3 wcs
-  
+  Clustering.kmeans cmpWordCount avgWordCount 2 wcs
+
+-- |Compare two word counts
 cmpWordCount :: [(String, Int)] -> [(String, Int)] -> Int
 cmpWordCount c1 c2 =
   let c1' = map (\(s, i) -> (s, [i])) (sortWordCount c1) in
   let c2' = map (\(s, i) -> (s, [i])) (sortWordCount c2) in
   let merged = mergeWordCounts c1' c2' in
   diff merged where
+
+    -- |Get the difference between two word counts
     diff :: [(String, [Int])] -> Int
     diff ((_, (i1 : i2 : _)) : rest) =
       (abs (i1 - i2)) + (diff rest)
     diff [] = 0
     diff (_ : rest) = diff rest
 
+    -- |Sort a word count alphabetically
     sortWordCount :: [(String, a)] -> [(String, a)]
     sortWordCount = sortBy (\(s, _) (s', _) -> compare s s')
 
+-- |Get the average for a list of word counts
 avgWordCount :: [[(String, Int)]] -> [(String, Int)]
 avgWordCount wcs =
   let wcs' = map (map (\(s, i) -> (s, [i]))) wcs in
   let merged = foldl mergeWordCounts [] wcs' in
-  map (\(s, is) -> (s, sum is)) merged
+  map (\(s, is) -> (s, (sum is) `div` (length is))) merged
 
+-- |Merge two word counts, must be sorted alphabetically
 mergeWordCounts :: [(String, [Int])] -> [(String, [Int])] -> [(String, [Int])]
 mergeWordCounts [] [] = []
 mergeWordCounts cs@(_:_) [] = cs
