d754106f67fc687b53985df3c311c7a6870699e2
Misha Wagner <mishajw@gmail.com>
2016-04-01 10:40:43 +0100

    KMeans clustering works

--- Clustering.hs
+++ Clustering.hs
@@ -1,13 +1,15 @@
 module Clustering where
 
+import Data.List
+
 -- |K-means clustering.
 -- |Takes Comparator, Averager, amount of clusters, and the list to cluster.
-kmeans :: (Num b) => (a -> a -> b) -> ([a] -> a) -> Int -> [a] -> [[a]]
+kmeans :: (Eq a, Ord b, Num b) => (a -> a -> b) -> ([a] -> a) -> Int -> [a] -> [[a]]
 kmeans cmp avg n as =
-  kmeans' cmp avg as (take n as) 10 where
+  kmeans' cmp avg as (take n as) 100 where
 
   -- |Also takes the current clusters, and iterations to go
-  kmeans' :: (Num b) => (a -> a -> b) -> ([a] -> a) -> [a] -> [a] -> Int -> [[a]]
+  kmeans' :: (Eq a, Ord b, Num b) => (a -> a -> b) -> ([a] -> a) -> [a] -> [a] -> Int -> [[a]]
   kmeans' cmp avg as cs 0 =
     clusterTo as cs cmp
   kmeans' cmp avg as cs iteration =
@@ -15,12 +17,24 @@ kmeans cmp avg n as =
     let newCenters = map avg clusters in
     kmeans' cmp avg as newCenters (iteration - 1)
 
-  clusterTo :: (Num b) => [a] -> [a] -> (a -> a -> b) -> [[a]]
-  clusterTo (a : as) cs cmp = []
+  clusterTo :: (Eq a, Ord b, Num b) => [a] -> [a] -> (a -> a -> b) -> [[a]]
+  clusterTo as cs cmp =
+    map (\(p, c) -> c) (clusterTo' as cs cmp) where
 
-kmeansTest = kmeans cmp avg 3 [1, 1, 2, 4, 4, 4, 6, 7, 8] where
-  cmp :: Integer -> Integer -> Integer
-  cmp a b = a - b
-  avg :: [Integer] -> Integer
-  avg as = ((sum as) / ((length as) :: Integer)) :: Integer
+    clusterTo' :: (Eq a, Ord b, Num b) => [a] -> [a] -> (a -> a -> b) -> [(a, [a])]
+    clusterTo' [] cs cmp = map (\c -> (c, [])) cs
+    clusterTo' (a : as) cs cmp =
+      -- Get the rest
+      let rest = clusterTo' as cs cmp in
+      -- Sort by distance to node and get the head
+      let ((centre, children) : rs) = sortBy (\(c, _) (c', _) -> compare (cmp a c) (cmp a c')) rest in
+      -- Append current to head
+      (centre, a : children) : rs
+
+kmeansTest = kmeans cmpInt avgInt 4 [1, 1, 3, 3, 10, 100, 10, 10, 200, 200, 200, 1, 1, 1, 1, 1] where
+  cmpInt :: Int -> Int -> Int
+  cmpInt a b = abs (a - b)
+  avgInt :: [Int] -> Int
+  avgInt as@(_:_) = ((sum as) `div` (length as)) :: Int
+  avgInt _ = 0
 
