b7c55385dd30f26157c867af62a0d2a1bd19b4dc
Misha Wagner <mishajw@gmail.com>
2016-04-04 18:12:39 +0100

    Runs text through clustering

--- Main.hs
+++ Main.hs
@@ -1,12 +1,11 @@
 module Main where
 
 import System.IO (readFile)
-import WordCountClustering (runOnText)
+import WordCountClustering (runOnText, wordCount)
 
 files = map (\i -> "test" ++ show i ++ ".txt") [1..4]
 
 main = do
   contents <- mapM readFile files
-  let clusters = runOnText contents
-  print clusters
+  print $ runOnText contents
 
