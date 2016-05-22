74ce19251db40ad9b76cedc72514309a45070a56
Misha Wagner <mishajw@gmail.com>
2016-05-22 11:32:10 +0100

    Combine Lib and Run files

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,6 @@ import Lib
 main = do
   args <- getArgs
   case args of
-    dir : amount : [] -> someFunc dir (read amount)
+    dir : amount : [] -> run dir (read amount)
     _ -> putStrLn "Incorrect use, need <directory> <amount of groups>"
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,41 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (run) where
 
-import Run
+import System.IO
+import System.Environment (getArgs)
+import System.Directory (getDirectoryContents)
+import System.FilePath ((</>))
 
-someFunc :: String -> Int -> IO ()
-someFunc = run
+import WordCounting (clusterTexts)
+import WordCountTypes (UncountedText(..), WordCount(..))
+
+run :: String -> Int -> IO ()
+run dir amount = do
+  putStrLn $ "Putting files from " ++ dir ++ " into " ++ show amount ++ " clusters"
+
+  files <- getFileNames dir
+  uts <- mapM getUncountedText files
+
+  let clusters = clusterTexts amount uts
+  
+  mapM_ (print . map wcTitle) clusters
+
+getFileNames :: FilePath -> IO [FilePath]
+getFileNames dir = do 
+  files <- getDirectoryContents dir 
+  return $ (map (dir ++) . filter ((/= '.') . head)) files
+
+getUncountedText :: FilePath -> IO UncountedText
+getUncountedText path = do
+  handle <- openFile path ReadMode
+  hSetEncoding handle char8
+  contents <- hGetContents handle
+
+  let ut = UncountedText {
+    utTitle = path
+  , utText = contents
+  }
+
+  -- hClose handle
+
+  return ut
+
