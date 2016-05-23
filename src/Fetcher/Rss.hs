80d27f66994a7a32276c5d45044ab930157ca5d6
Misha Wagner <mishajw@gmail.com>
2016-05-23 15:41:09 +0100

    Add rss links retrieval

--- src/Fetcher/Rss.hs
+++ src/Fetcher/Rss.hs
@@ -1,13 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Fetcher.Rss (fetchRssLinks) where
+module Fetcher.Rss (
+  getRssLinks
+, fetchRssLinks
+) where
 
-import System.IO (putStrLn)
+import System.IO (putStrLn, readFile)
 import Data.Char (isSpace)
 import Data.Text (unpack)
+import Data.List (lines)
 import Text.XML.Cursor (Cursor, content, child, element, (>=>), ($//))
 
 import Fetcher (getCursor)
+
+rssLinksFile :: FilePath
+rssLinksFile = "res/rss-feeds.txt"
+
+getRssLinks :: IO [String]
+getRssLinks = do
+  s <- readFile rssLinksFile
+  return $ lines s
+
+-- | Fetch the links found in a RSS URL
+fetchRssLinks :: String -> IO [String]
+fetchRssLinks url = do
+  cursor <- getCursor url
+  let elems = cursor $// findElements
+  let links = findLinks elems
+  return links
 
 -- | Get the elements from a cursor
 findElements :: Cursor -> [Cursor]
@@ -19,12 +39,4 @@ findLinks c =
   filter (not . null) .
   map ((filter $ not.isSpace) . unpack) $
     foldMap content c
-
--- | Fetch the links found in a RSS URL
-fetchRssLinks :: String -> IO [String]
-fetchRssLinks url = do
-  cursor <- getCursor url
-  let elems = cursor $// findElements
-  let links = findLinks elems
-  return links
 
