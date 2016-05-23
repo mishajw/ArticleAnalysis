82bfe597884d72d37fa0ed1a006d968f53d3e921
Misha Wagner <mishajw@gmail.com>
2016-05-23 15:41:25 +0100

    Add concurrent fetching function

--- src/Fetcher.hs
+++ src/Fetcher.hs
@@ -1,8 +1,13 @@
-module Fetcher (getCursor) where
+module Fetcher (getCursor, runOnUrls) where
 
 import Network.HTTP.Conduit (simpleHttp)
 import Text.HTML.DOM (parseLBS)
 import Text.XML.Cursor (Cursor, fromDocument)
+import Control.Concurrent.ParallelIO
+
+runOnUrls :: (String -> IO a) -> [String] -> IO [a]
+runOnUrls f urls = do
+  parallel $ map f urls
 
 -- | Get the cursor a URL
 getCursor :: String -> IO Cursor
