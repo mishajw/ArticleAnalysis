634d93c54eb344e464e5b9a3e46702c2b976c7d6
Misha Wagner <mishajw@gmail.com>
2016-05-23 17:29:48 +0100

    Change so only extracts article section

--- src/Fetcher/Article.hs
+++ src/Fetcher/Article.hs
@@ -2,14 +2,22 @@
 
 module Fetcher.Article where
 import Data.Text (unpack)
-import Text.XML.Cursor (descendant, content, child, element, checkName, ($//), (>=>))
+import Data.List (isInfixOf)
+import Text.XML.Cursor (Cursor, descendant, attributeIs, content, child, element, checkName, ($//), (>=>))
 
 import Fetcher (getCursor)
 
 fetchArticle :: String -> IO String
 fetchArticle url = do
   c <- getCursor url
-  let elems = c $// descendant
+  let elems = c $// getElements url
   let contents = foldMap content elems
   return $ concat . (map unpack) $ contents
+
+getElements :: String -> Cursor -> [Cursor]
+getElements url
+  | "bbc.co.uk"           `isInfixOf` url = attributeIs "class" "story-body" >=> descendant
+  | "theguardian.com"     `isInfixOf` url = attributeIs "itemprop" "articleBody" >=> descendant
+  | "independent.co.uk"   `isInfixOf` url = attributeIs "class" "main-content-column" >=> descendant
+  | otherwise = descendant
 
