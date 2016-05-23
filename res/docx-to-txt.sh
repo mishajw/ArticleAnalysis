df3c0c277613666740e89eed01e1a80fbc5b6f72
Misha Wagner <mishajw@gmail.com>
2016-05-23 15:16:54 +0100

    Remove old essay modifiers, and add RSS feeds list

--- res/docx-to-txt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-unzip -p "$1" word/document.xml | sed -e 's/<[^>]\{1,\}>//g; s/[^[:print:]]\{1,\}//g'
-
--- res/rename-all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./rename.sh ./emmas ./txts emma
-./rename.sh ./joes ./txts joe
-
