3a06350faf62145eb56e524e84bb1d3de909142e
Misha Wagner <mishajw@gmail.com>
2016-05-26 12:18:17 +0100

    Extract cluster ordering to a function

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,14 +9,15 @@ import Lib
 main = hspec $ do
   describe "Clustering" $ do
     it "can cluster basic integers correctly" $ do
-      let normalised = sortWith sum $
-                       map sort $
-                       clusterInts 2 [3, 2, 1, 7, 8, 9]
+      let normalised = orderIntCluster $ clusterInts 2 [3, 2, 1, 7, 8, 9]
 
       normalised `shouldBe` [[1, 2, 3], [7, 8, 9]]
 
 clusterInts :: Int -> [Int] -> [[Int]]
 clusterInts = kmeans cmpInt avgInt
+
+orderIntCluster :: [[Int]] -> [[Int]]
+orderIntCluster is = sortWith sum $ map sort is
 
 cmpInt :: Int -> Int -> Int
 cmpInt a b = abs (a - b)
