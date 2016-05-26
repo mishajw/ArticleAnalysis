b78574aca04905c53c4fd78239fd443b5beb4b81
Misha Wagner <mishajw@gmail.com>
2016-05-26 16:00:30 +0100

    Clean kmeans module

--- src/Cluster/Kmeans.hs
+++ src/Cluster/Kmeans.hs
@@ -4,13 +4,13 @@ import Data.List
 
 defaultIterations = 10
 
--- |K-means clustering.
--- |Takes Comparator, Averager, amount of clusters, and the list to cluster.
+-- | K-means clustering.
+-- | Takes Comparator, Averager, amount of clusters, and the list to cluster.
 kmeans :: (Eq a, Ord b, Num b) => (a -> a -> b) -> ([a] -> a) -> Int -> [a] -> [[a]]
 kmeans cmp avg n as =
   kmeans' cmp avg as (take n as) defaultIterations where
 
-  -- |Also takes the current clusters, and iterations to go
+  -- | Also takes the current clusters, and iterations to go
   kmeans' :: (Eq a, Ord b, Num b) => (a -> a -> b) -> ([a] -> a) -> [a] -> [a] -> Int -> [[a]]
   kmeans' cmp avg as cs 0 =
     clusterTo as cs cmp
@@ -32,12 +32,4 @@ kmeans cmp avg n as =
       let ((centre, children) : rs) = sortBy (\(c, _) (c', _) -> compare (cmp a c) (cmp a c')) rest in
       -- Append current to head
       (centre, a : children) : rs
-
-testKmeans = kmeans cmp avg 4 [1,2,3,7,8,9] where
-  
-  cmp :: Int -> Int -> Int
-  cmp a b = abs (a - b)
-
-  avg :: [Int] -> Int
-  avg is = (sum is) `div` (length is)
 
