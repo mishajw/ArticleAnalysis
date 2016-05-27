9a2b1357f8b4e66aa847d65c2607b1532362a5d6
Misha Wagner <mishajw@gmail.com>
2016-05-27 16:00:51 +0100

    Add function for inserting WordCount into database

--- src/DB.hs
+++ src/DB.hs
@@ -4,8 +4,11 @@ module DB where
 import Data.String.Utils (strip)
 import Data.List.Split (splitOn)
 import qualified Data.Text as DT (pack)
+import Data.Int (Int64)
 import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow
+
+import Cluster.Words
 
 defaultConnection = open "res/database.db"
 
@@ -19,6 +22,27 @@ runFile path = do
   statements <- readFile path
   let statementList = map DT.pack . filter (not.null) . map strip $ splitOn ";" statements
   conn <- defaultConnection
-  mapM ((execute_ conn) . Query) $ statementList
+  mapM_ (execute_ conn . Query) statementList
   return ()
+
+insertWordCount :: WordCount -> IO ()
+insertWordCount (WordCount t cs) = do
+  conn <- defaultConnection
+  execute conn "INSERT INTO page (name) VALUES (?)" $ Only t
+  pageId <- lastInsertRowId conn
+
+  mapM_ (\(w, c) -> do
+    print w
+    print c
+    execute conn "INSERT INTO word (word) VALUES (?)" (Only w)
+    wordId <- lastInsertRowId conn
+    execute conn "INSERT INTO page_word (page_id, word_id) VALUES (?, ?)" (PageWordRow pageId wordId)) cs
+
+data PageWordRow = PageWordRow Int64 Int64 deriving Show
+
+instance FromRow PageWordRow where
+  fromRow = PageWordRow <$> field <*> field
+
+instance ToRow PageWordRow where
+  toRow (PageWordRow pid wid) = toRow (pid, wid)
 
