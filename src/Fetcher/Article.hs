6f6b81db0b65c8297064e45b472dda0fadb080c8
Misha Wagner <mishajw@gmail.com>
2016-06-04 15:07:02 +0100

    Change article fetcher to not get script and style tags

--- src/Fetcher/Article.hs
+++ src/Fetcher/Article.hs
@@ -3,21 +3,21 @@
 module Fetcher.Article where
 import Data.Text (unpack)
 import Data.List (isInfixOf)
-import Text.XML.Cursor (Cursor, descendant, attributeIs, content, child, element, checkName, ($//), (>=>))
+import Text.XML.Cursor (Cursor, descendant, attributeIs, content, child, element, checkName, ($//), (>=>), ($|), (&//), (&|))
 
 import Fetcher (getCursor)
 
 fetchArticle :: String -> IO String
 fetchArticle url = do
   c <- getCursor url
-  let elems = c $// getElements url
+  let elems = c $// getElements url >=> descendant >=> checkName (not . (`elem` ["script", "style"])) >=> child
   let contents = foldMap content elems
   return $ concatMap unpack contents
 
 getElements :: String -> Cursor -> [Cursor]
 getElements url
-  | "bbc.co.uk"           `isInfixOf` url = attributeIs "class" "story-body" >=> descendant
-  | "theguardian.com"     `isInfixOf` url = attributeIs "itemprop" "articleBody" >=> descendant
-  | "independent.co.uk"   `isInfixOf` url = attributeIs "class" "main-content-column" >=> descendant
-  | otherwise = descendant
+  | "bbc.co.uk"         `isInfixOf` url = attributeIs "class"    "story-body"
+  | "theguardian.com"   `isInfixOf` url = attributeIs "itemprop" "articleBody"
+  | "independent.co.uk" `isInfixOf` url = attributeIs "class"    "main-content-column"
+  | otherwise = pure
 
